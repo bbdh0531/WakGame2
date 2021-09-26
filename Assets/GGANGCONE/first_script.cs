fc807f607ecc791310dc3b3d8ce1ef1c762d60e5
zihasoo <dlquddbs-@daum.net>
2021-09-26 16:36:44 +0900

    first

---
+++ Assets/GGANGCONE/first_script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class first_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello PANZEE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
